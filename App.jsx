db8dd9c8d38aa2c9ae8c2736a81301dce254910f
srchby <raidentsuji@gmail.com>
2025-05-11 22:45:20 -0300

    setup

---
+++ App.jsx
@@ -0,0 +1,31 @@
+// App.js
+import React from 'react';
+import { Button, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+const Stack = createStackNavigator();
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Bem-vindo ao Meu App!</Text>
+      <Button title="Ir para a Tela 2" onPress={() => navigation.navigate('Perfil')} />
+    </View>
+  );
+}
+function PerfilScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Olá, essa é a Tela 2 com seu perfil!</Text>
+    </View>
+  );
+}
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Perfil" component={PerfilScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
